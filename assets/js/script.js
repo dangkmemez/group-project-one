775f6832277d5c2cb41ac1498b50bcdd62f2d8b4
Erika Cannon <cannone.rva@gmail.com>
2022-04-13 14:32:10 -0400

    add year array for search parameter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,43 @@
 //handles recipes
+let recipeBtn = document.getElementsByClassName("recipe")[0];
+let recipeCont = document.getElementsByClassName('recContainer')[0];
+let list = recipeCont.querySelector('ol');
+let title = recipeCont.querySelector('h1');
+recipeBtn.addEventListener("click", getRandomRecipe);
+
+
+
+
+
+function getRandomRecipe(e) {
+    list.innerHTML = "";
+    title.innerHTML = "";
+    let api = "96a4968f21f744f9aac6e447181e2bf6"
+    var requestUrl = `https://api.spoonacular.com/recipes/random?number=1&apiKey=${api}`;
+    console.log(requestUrl.textContent)
+    fetch(requestUrl)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        // Using console.log to examine the data
+        console.log(data);
+        let returnRec = data.recipes[0];
+        let returnInstruct = returnRec.analyzedInstructions[0].steps;
+
+        console.log(returnInstruct);
+         //add title
+         title.innerHTML = returnRec.title;
+        for(let i = 0; i < returnInstruct.length; i++) {
+            console.log(returnInstruct[i].step, returnInstruct[i].number);
+            //add steps
+            let step = document.createElement('li');
+            step.classList.add("step");
+            step.innerHTML = returnInstruct[i].step;
+
+
+            list.append(step);
+
+        }
+      })
+    } 
--- assets/js/script2.js
+++ assets/js/script2.js
@@ -3,44 +3,54 @@
 //global variables
 var movieTitleContainerEl = document.querySelector("#movie-title");
 var movieButtonEl = document.querySelector("#movie");
+var movieCont = document.getElementsByClassName('movieContainer')[0];
+var movieTitle = movieCont.querySelector('h1');
 
 //array to insert random letter for movie titles
 const randomLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const randomYear = ["1980","1981","1982","1983","1984","1985","1986","1989","1989","1990","1991","1992","1993","1994","1995","1996","2000","2005","2010","2015"]
 var randomValue = randomLetter[Math.floor(randomLetter.length * Math.random())];
-console.log(randomValue);
+var randomYValue = randomYear[Math.floor(randomYear.length * Math.random())];
 
 
-
-//click event to get movie title from OMBd
-var buttonClickHandler = function(event) {
-
-
-};
+function getRandomValue() {
+  randomLetter[Math.floor(randomLetter.length * Math.random())];
+  randomYear[Math.floor(randomYear.length * Math.random())];
+  return randomLetter;
+}
 
 
 var getMovieTitle = function () {
-  //"t" is the parameter per OMDb api//
+  movieTitle.innerHTML = "";
+  getRandomValue();
+  
 
   var movieTitleUrl =
-    "https://www.omdbapi.com/?type=movie&t=" + randomValue + "&apikey=ce1eea9d";
-
+    "https://www.omdbapi.com/?type=movie&t=" + randomValue + "&y=" + randomYValue + "&apikey=ce1eea9d";
+    console.log(randomValue);
+    
   //make get request to url
   fetch(movieTitleUrl).then(function (response) {
-    //request was successful
-    if (response.ok) {
-      response.json().then(function (data) {
-        console.log(data);
-        displayMovieTitle(data, title, poster);
-      });
-    }
-  });
+    
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    console.log(data.Title);
+
+   
+
+    movieTitle.textContent = data.Title;
+
+    //loop through movie titles
+  //for (var i = 0; i < randomValue.length; i++) {
+
+   // movieTitle.appendChild(movieTitleContainerEl);
+  //}
+  })
 };
 
-var displayMovieTitle = function (title) {
-  //loop over movie titles
-  for (var i = 0; i < randomLetter.length; i++) {}
-};
 
 
 //event listener to button click
-movieButtonEl.addEventListener("click", buttonClickHandler);
+movieButtonEl.addEventListener("click", getMovieTitle);
